2372c5ee4580da67df3e0d3262e5ef559e735abf
punnatat <punnatat.tee@dome.tu.ac.th>
2026-04-28 21:33:41 +0700

    Evaluation_v1

--- TUwebsite/src/main/java/com/coursestu/central_portal/repository/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coursestu.central_portal.repository;
-
-import com.coursestu.central_portal.model.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
